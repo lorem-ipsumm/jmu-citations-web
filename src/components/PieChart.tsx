7dc682ecd5b1cd3cf065955fa552e8bdf7e21ddf
nick <morri2ne@dukes.jmu.edu>
2020-02-10 14:58:25 -0500

    ?

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -5,11 +5,20 @@ import { PieChart, Pie, Tooltip, } from 'recharts';
 /*
     This component handles displaying a pie chart of the data
 */
+
+/*
 const data01 = [
     { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
 ];
+*/
 
-export default class Loading extends React.Component<any, any>{
+
+interface MyProps {
+  location_data: any
+}
+
+export default class PieChar extends React.Component<MyProps, any>{
+
     constructor(props : any){
         super(props);
         this.state = {
@@ -18,13 +27,16 @@ export default class Loading extends React.Component<any, any>{
         }
     }
 
+    componentDidMount = () => {
+        console.log(this.props);
+    }
+
 
     render() {
         return(
             <div className="loading-wrapper wrapper">
                <PieChart width={400} height={400}>
-                   <Pie dataKey="value" isAnimationActive={true} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label />
-                   <Tooltip />
+                   <Pie dataKey="value" legendType={'square'} isAnimationActive={true} data={this.props.location_data} cx={200} cy={200} outerRadius={100} fill="#8884d8" label />
                </PieChart> 
             </div>
         );
